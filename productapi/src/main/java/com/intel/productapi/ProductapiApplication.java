514ac389405a96e7c83fde97836cb7a9d1b20d35
Administrator <Administrator@WINDOWS-BVQNF6J.mshome.net>
2022-02-02 09:21:26 +0530

    updated

--- productapi/src/main/java/com/intel/productapi/ProductapiApplication.java
+++ productapi/src/main/java/com/intel/productapi/ProductapiApplication.java
@@ -3,6 +3,7 @@ package com.intel.productapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//product ready to rock
 @SpringBootApplication
 public class ProductapiApplication {
 
