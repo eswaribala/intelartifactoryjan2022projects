be42ae4cc2ba51d7e36c58070c89be8d9d211039
Administrator <Administrator@WINDOWS-BVQNF6J.mshome.net>
2022-02-04 10:24:55 +0530

    updated

---
+++ remotejfrogproductapi/src/main/java/com/intel/productapi/ProductapiApplication.java
@@ -0,0 +1,15 @@
+package com.intel.productapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+//product ready to rock
+@SpringBootApplication
+public class ProductapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductapiApplication.class, args);
+	}
+
+}
